2f1d1e95cd7c963c6f130409e638e0c7186e44c7
Joseph Kim <joekimga@gmail.com>
2021-04-08 18:27:48 -0400

    gauge working

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,7 @@ function buildData(sample) {
 
 function buildPlots(sample) {
   d3.json("samples.json").then((data) => {
-    // console.log(data);
+    console.log(data);
     var samples = data.samples;
     var outputArray = samples.filter(sampleObject => sampleObject.id == sample);
     var output = outputArray[0];
@@ -42,7 +42,6 @@ function buildPlots(sample) {
     // Plot the chart to a div tag with id "bar-plot"
     Plotly.newPlot("bar", bar_data, bar_layout);
 
-    
     // Bubble chart
     var trace1 = {
       y: sample_values,
@@ -55,54 +54,65 @@ function buildPlots(sample) {
         colorscale: [[0, '#4b4ba9'], [.2, '#54d2b0'],[.2, '#7fe36b'],[.5, '#c0ea6e'],[.5, '#99752b'],[1, '#d7c7b9']]
       }
   };
-  var data = [trace1];  
+  var trace_data = [trace1];  
   var layout = {
       xaxis: {title: 'OTU ID'},
       showlegend: false,
   };
-  Plotly.newPlot('bubble', data, layout);
+  Plotly.newPlot('bubble', trace_data, layout);
   //////////// End of Bubble  /////////////////
 
   ///////// GAUGE  //////////////
+  console.log(output);
+  // var wfreq = output.metadata.wfreq;
+  // var wfreq = output.metadata[6];
+  var metadata = data.metadata;
+  var outputArraywfreq = metadata.filter(wfreqObject => wfreqObject.id === parseInt(sample));
+  var wfreq = outputArraywfreq[0].wfreq;
+  console.log(outputArraywfreq, wfreq);
+  // var outputWfreq = outputArraywfreq[0];
+  // var wfreq_values = outputWfreq.wfreq_values;
+  // var wfreq = outputWfreq.wfreq;
+  console.log(wfreq);
+
   var gauge = [
     {
+      // domain: { x: [0, 1], y: [0, 1] },
       domain: { x: otu_ids, y: sample_values },
-      value: 940,
-      title: { text: "Belly Button Washing Frequency" },
-      subtitle: { text: "Scrubs per Week"},
+      value: wfreq,
+      // value: 490,
+      title: {text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week"},
       type: "indicator",
       mode: "gauge+number+delta",
       delta: { reference: 380 },
       gauge: {
         axis: { range: [null, 9] },
         steps: [
-          { range: [0, 1], color: "lightgray"},
-          { range: [1, 2], color: "gray" },
-          { range: [2, 3], color: "lightgray" },
-          { range: [3, 4], color: "gray" },
-          { range: [4, 5], color: "lightgray" },
-          { range: [5, 6], color: "gray" },
-          { range: [6, 7], color: "lightgray" },
-          { range: [7, 8], color: "gray" },
-          { range: [8, 9], color: "lightgray" }
-        
+          { range: [0, 1], color: "lightblue"},
+          { range: [1, 2], color: "lightgray" },
+          { range: [2, 3], color: "lightblue" },
+          { range: [3, 4], color: "lightgray" },
+          { range: [4, 5], color: "lightblue" },
+          { range: [5, 6], color: "lightgray" },
+          { range: [6, 7], color: "lightblue" },
+          { range: [7, 8], color: "lightgray" },
+          { range: [8, 9], color: "lightblue" },
+
         ],
         threshold: {
           line: { color: "red", width: 4 },
           thickness: 0.75,
-          value: 490
+          value: wfreq
+          // value: 490,          
         }
       }
     }
   ];
   
-  var gauge_layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+  var gauge_layout = { width: 460, height: 450, margin: { t: 0, b: 0 } };
   Plotly.newPlot('gauge', gauge, gauge_layout);
 
-
-
-
-
+////////  GAUGE END  //////////
 
 
 
